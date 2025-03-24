c3dd0a860d362ff13bd3100bdd4a4f7a45ff78a0
Tiago Ledur <tiagoasledur@gmail.com>
2025-03-24 15:28:58 -0300

    Criado entidade PedidoCompra

---
+++ src/main/java/com/example/stock_control_api/model/PedidoCompra.java
@@ -0,0 +1,34 @@
+package com.example.stock_control_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pedidos_compra")
+public class PedidoCompra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "fornecedor_id", nullable = true)
+    private Fornecedor fornecedor;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedidoCompra> itens;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataPedido = LocalDateTime.now();
+
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+}
+
