0ea1887d4abe323ed21326bd5c3ef6cd3aae4da2
Tiago Ledur <tiagoasledur@gmail.com>
2025-04-08 14:56:23 -0300

    Mudando nome para apenas "Movimentacao"

--- src/main/java/com/example/stock_control_api/model/Movimentacao.java
+++ src/main/java/com/example/stock_control_api/model/Movimentacao.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class MovimentacaoEstoque {
+public class Movimentacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
