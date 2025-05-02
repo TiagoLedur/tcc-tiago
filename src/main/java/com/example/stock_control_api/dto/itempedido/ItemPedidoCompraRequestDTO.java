0d7cbc78dab392f857c5ceac6b85532f5da5ddc2
Tiago Ledur <tiagoasledur@gmail.com>
2025-05-02 14:40:57 -0300

    Updating ItemPedidoCompra's DTO

--- src/main/java/com/example/stock_control_api/dto/itempedido/ItemPedidoCompraRequestDTO.java
+++ src/main/java/com/example/stock_control_api/dto/itempedido/ItemPedidoCompraRequestDTO.java
@@ -1,14 +1,19 @@
 package com.example.stock_control_api.dto.itempedido;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ItemPedidoCompraRequestDTO {
+
+    @NotNull(message = "O ID do pedido é obrigatório.")
+    private Long pedidoId;
 
     @NotNull(message = "O ID do ingrediente é obrigatório.")
     private Long ingredienteId;
