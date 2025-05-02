a2313e4712d02c88da0e7e6b9e11932ebd8a009c
Tiago Ledur <tiagoasledur@gmail.com>
2025-05-02 14:40:50 -0300

    Updating ItemPedidoCompra's Mapper

--- src/main/java/com/example/stock_control_api/mapper/ItemPedidoCompraMapper.java
+++ src/main/java/com/example/stock_control_api/mapper/ItemPedidoCompraMapper.java
@@ -4,18 +4,21 @@ import com.example.stock_control_api.dto.itempedido.ItemPedidoCompraRequestDTO;
 import com.example.stock_control_api.dto.itempedido.ItemPedidoCompraResponseDTO;
 import com.example.stock_control_api.model.Ingrediente;
 import com.example.stock_control_api.model.ItemPedidoCompra;
+import com.example.stock_control_api.model.PedidoCompra;
 
 public class ItemPedidoCompraMapper {
 
     private ItemPedidoCompraMapper() {}
 
-    public static ItemPedidoCompra toEntity(ItemPedidoCompraRequestDTO dto, Ingrediente ingrediente) {
+    public static ItemPedidoCompra toEntity(ItemPedidoCompraRequestDTO dto, PedidoCompra pedido, Ingrediente ingrediente) {
         return ItemPedidoCompra.builder()
+                .pedido(pedido)
                 .ingrediente(ingrediente)
                 .quantidade(dto.getQuantidade())
                 .precoUnitario(dto.getPrecoUnitario())
                 .build();
     }
+
 
     public static ItemPedidoCompraResponseDTO toDTO(ItemPedidoCompra entity) {
         return ItemPedidoCompraResponseDTO.builder()
