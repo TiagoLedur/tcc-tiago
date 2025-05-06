18cf111744cc1280338208b3e2e3e4fccf511cb5
Tiago Ledur <tiagoasledur@gmail.com>
2025-05-06 13:59:33 -0300

    Updating LoteIngrediente's service

--- src/main/java/com/example/stock_control_api/service/LoteIngredienteService.java
+++ src/main/java/com/example/stock_control_api/service/LoteIngredienteService.java
@@ -1,34 +1,47 @@
 package com.example.stock_control_api.service;
 
+import com.example.stock_control_api.dto.lote.*;
+import com.example.stock_control_api.mapper.LoteIngredienteMapper;
+import com.example.stock_control_api.model.Ingrediente;
 import com.example.stock_control_api.model.LoteIngrediente;
+import com.example.stock_control_api.repository.IngredienteRepository;
 import com.example.stock_control_api.repository.LoteIngredienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LoteIngredienteService {
 
     private final LoteIngredienteRepository loteIngredienteRepository;
+    private final IngredienteRepository ingredienteRepository;
 
-    public List<LoteIngrediente> list() {
-        return loteIngredienteRepository.findAll();
+    public List<LoteIngredienteResponseDTO> list() {
+        return loteIngredienteRepository.findAll()
+                .stream()
+                .map(LoteIngredienteMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
-    public Optional<LoteIngrediente> findById(Long id) {
-        return loteIngredienteRepository.findById(id);
+    public LoteIngredienteResponseDTO findById(Long id) {
+        return loteIngredienteRepository.findById(id)
+                .map(LoteIngredienteMapper::toDTO)
+                .orElseThrow(() -> new NoSuchElementException("Lote não encontrado"));
     }
 
-    public LoteIngrediente save(LoteIngrediente categoria) {
-        return loteIngredienteRepository.save(categoria);
+    public LoteIngredienteResponseDTO save(LoteIngredienteRequestDTO dto) {
+        Ingrediente ingrediente = ingredienteRepository.findById(dto.getIngredienteId())
+                .orElseThrow(() -> new NoSuchElementException("Ingrediente não encontrado"));
+
+        LoteIngrediente entity = LoteIngredienteMapper.toEntity(dto, ingrediente);
+        return LoteIngredienteMapper.toDTO(loteIngredienteRepository.save(entity));
     }
 
     public void delete(Long id) {
         loteIngredienteRepository.deleteById(id);
     }
-
 }
-
