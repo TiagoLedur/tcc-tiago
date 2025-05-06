65b9137d4e2834b90c6157ede030d2eab875479d
Tiago Ledur <tiagoasledur@gmail.com>
2025-05-06 13:59:21 -0300

    Updating LoteIngrediente's controller

--- src/main/java/com/example/stock_control_api/controller/LoteIngredienteController.java
+++ src/main/java/com/example/stock_control_api/controller/LoteIngredienteController.java
@@ -1,42 +1,34 @@
 package com.example.stock_control_api.controller;
 
-import com.example.stock_control_api.model.LoteIngrediente;
+import com.example.stock_control_api.dto.lote.*;
 import com.example.stock_control_api.service.LoteIngredienteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/lotes")
+@RequestMapping("/lote-ingredientes")
 @RequiredArgsConstructor
 public class LoteIngredienteController {
 
     private final LoteIngredienteService loteIngredienteService;
 
     @GetMapping
-    public List<LoteIngrediente> listAll() {
-        return loteIngredienteService.list();
+    public ResponseEntity<List<LoteIngredienteResponseDTO>> list() {
+        return ResponseEntity.ok(loteIngredienteService.list());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<LoteIngrediente>> findById(@PathVariable Long id) {
+    public ResponseEntity<LoteIngredienteResponseDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(loteIngredienteService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<LoteIngrediente> create(@RequestBody LoteIngrediente loteIngrediente) {
-        LoteIngrediente novoLote = loteIngredienteService.save(loteIngrediente);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novoLote);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<LoteIngrediente> update(@PathVariable Long id, @RequestBody LoteIngrediente loteIngrediente) {
-        loteIngrediente.setId(id);
-        return ResponseEntity.ok(loteIngredienteService.save(loteIngrediente));
+    public ResponseEntity<LoteIngredienteResponseDTO> save(@Valid @RequestBody LoteIngredienteRequestDTO dto) {
+        return ResponseEntity.ok(loteIngredienteService.save(dto));
     }
 
     @DeleteMapping("/{id}")
