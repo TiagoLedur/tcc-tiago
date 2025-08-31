f88d0c04f6d03178752fedcd68a70d0893bc4811
Tiago Ledur <tiagoasledur@gmail.com>
2025-08-31 13:58:47 -0300

    Excluding migration

--- src/main/resources/db/migration/V2__create_movimentacao_estoque_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE movimentacoes_estoque (
-    id BIGSERIAL PRIMARY KEY,
-    ingrediente_id BIGINT REFERENCES ingredientes(id) ON DELETE CASCADE,
-    tipo VARCHAR(20) NOT NULL CHECK (tipo IN ('ENTRADA', 'SAIDA')),
-    quantidade DECIMAL(10,2) NOT NULL,
-    observacao VARCHAR(500),
-    data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
