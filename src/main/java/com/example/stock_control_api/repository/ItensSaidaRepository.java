7807fbb93124e02c53541512d48ac5c09718ebe8
Tiago Ledur <tiagoasledur@gmail.com>
2025-09-14 15:24:50 -0300

    Removing unused import

--- src/main/java/com/example/stock_control_api/repository/ItensSaidaRepository.java
+++ src/main/java/com/example/stock_control_api/repository/ItensSaidaRepository.java
@@ -1,6 +1,6 @@
 package com.example.stock_control_api.repository;
 
-import com.example.stock_control_api.model.ItensEntrada;
+
 import com.example.stock_control_api.model.ItensSaida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/stock_control_api/repository/SaidaRepository.java
+++ src/main/java/com/example/stock_control_api/repository/SaidaRepository.java
@@ -1,6 +1,5 @@
 package com.example.stock_control_api.repository;
 
-import com.example.stock_control_api.model.ItensSaida;
 import com.example.stock_control_api.model.Saida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
