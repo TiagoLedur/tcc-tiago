0628436cbba18874182406e1c2efb79175e7c8cd
Tiago Ledur <tiagoasledur@gmail.com>
2025-09-14 15:24:04 -0300

    Creating new repositories

---
+++ src/main/java/com/example/stock_control_api/repository/RelatorioRepository.java
@@ -0,0 +1,9 @@
+package com.example.stock_control_api.repository;
+
+import com.example.stock_control_api.model.Relatorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RelatorioRepository extends JpaRepository<Relatorio, Long> {
+}
---
+++ src/main/java/com/example/stock_control_api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.stock_control_api.repository;
+
+import com.example.stock_control_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
