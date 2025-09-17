700719504d30915e584700a0a73f8da22a8f9ee2
Tiago Ledur <tiagoasledur@gmail.com>
2025-09-17 20:21:42 -0300

    Adding "criadoEm" field

--- src/main/java/com/example/stock_control_api/dto/relatorio/RelatorioRequestDTO.java
+++ src/main/java/com/example/stock_control_api/dto/relatorio/RelatorioRequestDTO.java
@@ -2,8 +2,11 @@ package com.example.stock_control_api.dto.relatorio;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class RelatorioRequestDTO {
     private String tipo;
     private Long usuarioId;
+    private LocalDateTime criadoEm;
 }
