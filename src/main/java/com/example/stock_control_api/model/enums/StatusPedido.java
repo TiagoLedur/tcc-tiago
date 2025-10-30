c0702eb02c84c90b3a59e4e8ddba7eaa252db60d
Tiago Ledur <tiagoasledur@gmail.com>
2025-10-30 14:37:02 -0300

    Removing unused enums

--- src/main/java/com/example/stock_control_api/model/enums/StatusPedido.java
+++
@@ -1,7 +0,0 @@
-package com.example.stock_control_api.model.enums;
-public enum StatusPedido {
-    PENDENTE,
-    RECEBIDO,
-    CANCELADO
-}
-
--- src/main/java/com/example/stock_control_api/model/enums/TipoMovimentacao.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.stock_control_api.model.enums;
-public enum TipoMovimentacao {
-    ENTRADA,
-    SAIDA
-}
-
